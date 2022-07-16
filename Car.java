758c447895e96a3620cddd8baed78b9fdae35d37
Ritam Das <106969260+ritam332@users.noreply.github.com>
2022-07-16 12:39:13 +0530

    Update Car.java

--- Car.java
+++ Car.java
@@ -20,4 +20,19 @@ public class Car {
 	public String getModel() {
 		return this.model;
 	}
+	
+	
+	//..................Main Method...............................
+	public class Main {
+
+	public static void main(String[] args) {
+		Car porsche= new Car();
+		Car holden= new Car();
+		System.out.println("Model = "+porsche.getModel());
+		porsche.setModel("carrera");
+		System.out.println("Model = "+porsche.getModel());
+		porsche.setModel("911");
+		System.out.println("Model = "+porsche.getModel());
+
+	}
 }
